d3fee8c5d2a4810fbceee8b3f0f0ed6d27affa22
Deian Radev <deian.m.radev@gmail.com>
2020-02-01 13:48:44 -0500

    installed ngx-auth-firebaseui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [NgxAuthFirebaseUIModule.forRoot({
+    apiKey: 'AIzaSyD1DB06aPKlpPrAkh4pnY_fCCpIwj79pqM',
+    authDomain: 'bellyfull-631b3.firebaseapp.com',
+    databaseURL: 'https://bellyfull-631b3.firebaseio.com',
+    projectId: 'bellyfull-631b3',
+    storageBucket: 'bellyfull-631b3.appspot.com',
+    messagingSenderId: '440145801624',
+    appId: '1:440145801624:web:b75667eadc8527912ffbed',
+    measurementId: 'G-33H17HRXH4'
+  }),
+
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
